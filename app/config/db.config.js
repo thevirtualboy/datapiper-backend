94de744843607d04634e00643c9f493626e6bdd9
Peter Fontana <dude0508@gmail.com>
2022-04-20 01:05:57 -0400

    new stuff

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Kajimmy11!",
-    DB: "mydb",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "bd4df1f3f38077",
+    PASSWORD: "a355ce05",
+    DB: "heroku_c65f0774abccec4",
     dialect: "mysql",
     pool: {
       max: 5,
